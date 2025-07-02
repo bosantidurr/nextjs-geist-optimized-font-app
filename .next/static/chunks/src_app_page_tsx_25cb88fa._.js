6ffa184767a35bfadec3ad333dba8d5a09f495d6
bosantidurr <bosantidurr@gmail.com>
2025-07-02 18:38:30 +0700

    Add .next/static/chunks/src_app_page_tsx_25cb88fa._.js 1751456309749

---
+++ .next/static/chunks/src_app_page_tsx_25cb88fa._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_43cf3087._.js",
+  "static/chunks/src_4ca1d79d._.js"
+],
+    source: "dynamic"
+});
